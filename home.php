91be4a1ec3f8e35338fd9590ced21b4865afa2da
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-03-19 17:35:16 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,2 @@
-<span>Logged in as <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?></span>
+<span>Logged in as <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?></span> &nbsp;&nbsp;  
+<h1><a href="logout_google.php" onClick="location.href='logout_facebook.php'">Logout</a></h1>
