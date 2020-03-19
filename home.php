73c6693b4106c120d241ada02db650c44e5b8689
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-03-19 16:45:38 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,10 +1,43 @@
-<html> 
+<?php
+$load = (isset($_GET['page'])&& $_GET['page'] !='')? $_GET['page'] : '';
+
+include('google_config/google_read.php');
+if($login_button == true){
+    include('facebook_config/facebook_read.php');
+}
+?>
+<!DOCTYPE html>
+<html>
     <head>
-      <title>  API Exercise 1 </title>
-      <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-<body>
-  <div class="home_body">
-  </div>
-</body>
+		<title> Sample Movies Site </title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+    <body>
+	    <p style="color: white;"> Logged in as <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </p>
+        <div id="navigation_body">                                  
+            <div class="navbar">
+                <ul>
+                    <li><a href="home.php?page=#live"> Noew Showing </a></li>
+                    <li><a href="home.php?page=#coming_soon"> Coming Soon </a></li>
+                    <li><a href="home.php?page=movies"> Search Movies </a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <?php
+                switch($load){
+                    case 'movies':
+                        require_once('movie_list.php');
+                    break;
+                    case 'show-movie':
+                        require_once('movie_profile.php');
+                    break;
+                    default:
+                        require_once('movie_list.php');
+                    break;
+                }
+            ?>
+        </div>
+    </body>
+    <a href="google_logout.php" onClick="location.href='facebook_logout.php'">Logout</a>
 </html>
