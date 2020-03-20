3d6ff869a449ab774257b9b5b594024c4c63e33f
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-03-20 14:07:58 +0800

    Update show.php

--- show.php
+++ show.php
@@ -21,7 +21,7 @@ $search = $_POST['search'];
     <div class="main_body"><br/>
 <h1> API Endterm </h1>
 
-	<form action="index.php?loadnav=list" method="POST">
+	<form action="index.php?page=list" method="POST">
 	Search:<input type="text" name="search" placeholder="Search Product Name">
 		<button type="submit" name="submit" >Search</button>
 	</form>
@@ -35,7 +35,7 @@ $search = $_POST['search'];
 foreach($list as $result){
     ?>
     <tr>
-        <td> <a href="index.php?loadnav=profile&id=<?php echo $result['id'];?>"> <?php echo $result['name']; ?> </a> </td>
+        <td> <a href="index.php?page=profile&id=<?php echo $result['id'];?>"> <?php echo $result['name']; ?> </a> </td>
 	<td><?php echo $result['category_name'];?></td>   
     </tr>
 <?php
